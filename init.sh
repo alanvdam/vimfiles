9236ba15cb38b3b29c1f561d913b0d7e65aa68d9
Alan van Dam <alan@vandamnet.nl>
2013-04-19 23:53:31 +0200

    command-t needs to run 'rake make'

--- init.sh
+++ init.sh
@@ -1,3 +1,9 @@
 #!/bin/bash
 
-git submodule init && git submodule update && ln -s ~/.vim/vimrc ~/.vimrc
+# for the command t plugin you need to run rake make
+
+git submodule init && \
+git submodule update && \
+ln -s ~/.vim/vimrc ~/.vimrc && \
+cd bundle/command-t && \
+rake make
