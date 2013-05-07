eb63219219d9fedbd7b91565b117728ed68be563
Alan van Dam <alan@vandamnet.nl>
2013-05-07 20:33:36 +0200

    Tagbar needs ctags

--- init.sh
+++ init.sh
@@ -1,9 +1,11 @@
 #!/bin/bash
 
 # for the command t plugin you need to run rake make
+# ctags is needed by tagbar
 
 git submodule init && \
 git submodule update && \
 ln -s ~/.vim/vimrc ~/.vimrc && \
 cd bundle/command-t && \
-rake make
+rake make \
+brew install ctags
