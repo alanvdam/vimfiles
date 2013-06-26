036b45f815553451f1b08d5dcacee21affac9998
Alan van Dam <alan@vandamnet.nl>
2013-06-26 10:36:42 +0200

    Improved init script

--- init.sh
+++ init.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-git submodule init && git submodule update                       # init submodules
-[[ ! -e "~/.vim/vimrc" ]] || ln -s ~/.vim/vimrc ~/.vimrc         # link vimrc file
-[[ type rake &> /dev/null ]] || cd bundle/command-t && rake make # for the command t plugin you need to run rake make
-[[ type brew &> /dev/null ]] || brew install ctags               # ctags is needed by tagbar
-[[ type brew &> /dev/null ]] || brew install ack                 # ack is needed by Ack
+git submodule init && git submodule update                  # init submodules
+[[ -e ~/.vimrc ]] || ln -s ~/.vim/vimrc ~/.vimrc            # link vimrc file
+type rake &> /dev/null && cd bundle/command-t && rake make  # for the command t plugin you need to run rake make
+type brew &> /dev/null && brew install ctags                # ctags is needed by tagbar
+type brew &> /dev/null && brew install ack                  # ack is needed by Ack
