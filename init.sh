0bc9f77f38f33e080431334954f1accdcd36cfd1
Alan van Dam <alan@vandamnet.nl>
2013-07-09 16:12:13 +0200

    Run BundleInstall (vundle) in init script

--- init.sh
+++ init.sh
@@ -2,8 +2,7 @@
 
 git submodule init && git submodule update                  # init submodules
 [[ -e ~/.vimrc ]] || ln -s ~/.vim/vimrc ~/.vimrc            # link vimrc file
+vim +BundleInstall +qall
 type rake &> /dev/null && cd bundle/Command-T && rake make  # for the command t plugin you need to run rake make
 type brew &> /dev/null && brew install ctags                # ctags is needed by tagbar
 type brew &> /dev/null && brew install ack                  # ack is needed by Ack
-
-#vim -u ~/.vimrc.bundles +BundleInstall +qa
